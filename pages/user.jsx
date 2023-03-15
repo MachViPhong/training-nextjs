20b1354b74ba510f06d6aed68c536f2633e52f09
Mach Vi Phong <machviphong21042001@gmail.com>
2023-03-15 16:41:10 +0700

    init user page

---
+++ pages/user.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const user = () => {
+  return (
+    <>
+    <div>user Page Test</div>
+    <h1 className='fs1 text-success'>Mạch Vi Phong</h1>
+    <p className='description'>Sinh viên trường ĐH Khoa học tự nhiên</p>
+    </>
+  )
+}
+
+export default user
